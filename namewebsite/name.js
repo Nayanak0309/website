2b754373bacf52fd9c8072d8ed427d9164ad487f
Nayanak0309 <nayanaramanth2003@gmail.com>
2025-08-21 20:05:35 +0530

    new update

--- namewebsite/name.js
+++ namewebsite/name.js
@@ -1,5 +1,7 @@
 const btnElement = document.querySelector('button');
-const spanElement = document.querySelector('#udpdateContent');
+const spanElement = document.getElementById('updateContent');
 btnElement.onclick= function() {
     const yourName = prompt('What is your name?');
+    spanElement.textContent = `Hello, ${yourName}!`;
+    spanElement.style.color = 'blue';
 }
